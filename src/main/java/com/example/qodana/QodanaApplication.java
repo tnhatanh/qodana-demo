6ffec0d0ab5f124a178630c2437fdad2e26d147b
lqnhu <lqnhu-tma>
2024-03-06 17:11:24 +0700

    fix error

--- src/main/java/com/example/qodana/QodanaApplication.java
+++ src/main/java/com/example/qodana/QodanaApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class QodanaApplication {
 
-	Test nonsensical line
 	public static void main(String[] args) {
 		SpringApplication.run(QodanaApplication.class, args);
 	}
