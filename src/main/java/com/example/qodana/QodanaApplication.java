4365ae988cf09206d7ca771a8484f37e89b5abb3
lqnhu <lqnhu-tma>
2024-03-06 14:18:39 +0700

    init project

---
+++ src/main/java/com/example/qodana/QodanaApplication.java
@@ -0,0 +1,13 @@
+package com.example.qodana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QodanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QodanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/qodana/QodanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qodana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QodanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
